7a4edfa457e296dbdf183986350ade6a5f6d8011
rajyavardhan <rajyavardhans805>
2025-05-19 14:01:33 +0530

    box task

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const boxes = document.querySelectorAll('.box');
 
 boxes.forEach (box  => {
     box.addEventListener('click', () => {
-    box.style.backgroundColor = 'red';
+    // box.style.backgroundColor = 'red';
+    // box.style.border = '4px solid';
+    box.style.backgroundImage = "url('https://plus.unsplash.com/premium_photo-1669748156838-33fda144e8dd?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmVhdXRpZnVsJTIwbmF0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=3000&q=60')";
+    box.style.backgroundSize = 'cover';
+    box.style.backgroundRepeat = 'no-repeat';
     });
 });
